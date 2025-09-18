0de911b401ad0fa6830872d833615b3ec8f1a938
ripureityu <tanaka.yuudai@it-mirai-h.ibk.ed.jp>
2025-09-18 18:59:31 +0900

    一旦

--- train.py
+++ train.py
@@ -1,4 +1,26 @@
 import cv2
+import engineio
+
+# import http.client
+# http.client.HTTPConnection.debuglevel = 1
+
+eio = engineio.Client(
+    # logger=True,
+)
+
+@eio.on("connect")
+def connect():
+    print("connect")
+
+@eio.on("disconnect")
+def disconnect():
+    print("disconnect")
+
+@eio.on("message")
+def disconnect(msg):
+    print("message: ", msg)
+
+eio.connect("http://localhost:3000", transports=["polling"])
 
 # カメラを開く
 cap = cv2.VideoCapture(0)
@@ -6,8 +28,17 @@ cap = cv2.VideoCapture(0)
 # 画像をキャプチャする
 ret, frame = cap.read()
 
-# 画像を保存する
-cv2.imwrite("image.jpg", frame)
+#return ,image
+
+if ret:#取得できたら
+    # 画像を保存する
+    _, img_bytes =cv2.imencode(".jpg", frame)
+    eio.send(img_bytes.tobytes())
+else:
+    print("---カメラから画像を取得できませんでした---")
 
 # カメラを閉じる
 cap.release()
+
+eio.wait()
+
